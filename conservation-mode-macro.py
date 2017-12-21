b6e812e4e71b69eb3c9280d3e1b8e7fbaeec36d8
Alex Jiang <dashuai99@gmail.com>
2017-12-20 20:34:13 -0600

    modify timing to try to fail less often

--- conservation-mode-macro.py
+++ conservation-mode-macro.py
@@ -18,30 +18,31 @@ keyboard.type('companion')
 time.sleep(1) # wait a longer for the search result to show up
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
-time.sleep(5) # wait for the app to load
+time.sleep(10) # wait for the app to load
 
 # tab-cycle to the hardware settings menu
 for i in range(12):
+    time.sleep(0.2)
     keyboard.press(Key.tab)
     keyboard.release(Key.tab)
-    time.sleep(0.2)
+time.sleep(0.5)
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
-time.sleep(0.5)
 
 # tab-cycle to the power menu
 for i in range(9):
+    time.sleep(0.2)
     keyboard.press(Key.tab)
     keyboard.release(Key.tab)
-    time.sleep(0.2)
+time.sleep(0.5)
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
-time.sleep(0.5)
 
 # tab-cycle to the conservation mode option
 for i in range(5):
+    time.sleep(0.2)
     keyboard.press(Key.tab)
     keyboard.release(Key.tab)
-    time.sleep(0.2)
+time.sleep(0.5)
 keyboard.press(Key.space)
 keyboard.release(Key.space)
